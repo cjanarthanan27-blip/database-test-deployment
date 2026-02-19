cb3a21e0424a2a434959b9282bd05c65021ad76a
Deployer <deploy@example.com>
2026-02-19 22:28:55 +0530

    Refine catch-all URL to exclude static files

--- rathinamHR/urls.py
+++ rathinamHR/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.water_tracker.backend.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('<path:path>', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!admin|api|static).*$', TemplateView.as_view(template_name='index.html')),
 ]
