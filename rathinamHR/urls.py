8fe99f367f891cf1cefa0ef02ce7fa1a55bff78b
Deployer <deploy@example.com>
2026-02-19 21:52:39 +0530

    Configure Django to serve React frontend

--- rathinamHR/urls.py
+++ rathinamHR/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.water_tracker.backend.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('<path:path>', TemplateView.as_view(template_name='index.html')),
 ]
